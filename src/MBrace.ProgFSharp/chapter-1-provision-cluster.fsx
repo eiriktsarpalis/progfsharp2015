c20184fcdf97ad46e8378147b6c22de674435c96
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-12-03 01:23:13 +0200

    use nullary operator for undefined values

--- src/MBrace.ProgFSharp/chapter-1-provision-cluster.fsx
+++ src/MBrace.ProgFSharp/chapter-1-provision-cluster.fsx
@@ -8,22 +8,22 @@ open MBrace.Azure.Management
 
 // You can download your publication settings file at 
 //     https://manage.windowsazure.com/publishsettings
-let pubSettingsFile : string = __add_me__
+let pubSettingsFile : string = (!?)
 
 // If your publication settings defines more than one subscription,
 // you will need to specify which one you will be using here.
-let subscriptionId : string option = __add_me__
+let subscriptionId : string option = (!?)
 
 // Your prefered Azure service name for the cluster.
 // NB: must be a valid DNS prefix unique across Azure.
-let clusterName : string = __add_me__
+let clusterName : string = (!?)
 
 // Your prefered Azure region. Assign this to a data center close to your location.
-let region : Region = __add_me__
+let region : Region = (!?)
 // Your prefered VM size
-let vmSize : VMSize = __add_me__
+let vmSize : VMSize = (!?)
 // Your prefered cluster count
-let vmCount = __add_me__
+let vmCount : int = (!?)
 
 // Update your config file with current cloud settings
 Config.UpdateConfig(pubSettingsFile, subscriptionId, clusterName, region, vmSize, vmCount)
--- src/MBrace.ProgFSharp/utils/utils.fsx
+++ src/MBrace.ProgFSharp/utils/utils.fsx
@@ -1,7 +1,9 @@
 ﻿namespace global
 
+#nowarn "1189"
+
 [<AutoOpen>]
 module Utils =
 
     /// Please add an implementation here
-    let __add_me__<'T> : 'T = raise <| new System.NotImplementedException("Don't forget to add an implementation!")
+    let (!?) <'T> : 'T = raise <| new System.NotImplementedException("Don't forget to add an implementation!")
