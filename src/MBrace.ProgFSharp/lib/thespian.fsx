4227538ae3e29bfd41dfcc85271a87237103a258
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-12-05 14:47:58 +0200

    incorporate thespian in tutorials

---
+++ src/MBrace.ProgFSharp/lib/thespian.fsx
@@ -0,0 +1,36 @@
+﻿#I "../../../packages/MBrace.Thespian/tools" 
+#I "../../../packages/Streams/lib/net45" 
+#r "../../../packages/Streams/lib/net45/Streams.dll"
+#I "../../../packages/MBrace.Flow/lib/net45" 
+#r "../../../packages/MBrace.Flow/lib/net45/MBrace.Flow.dll"
+#load "../../../packages/MBrace.Thespian/MBrace.Thespian.fsx"
+
+namespace global
+
+module Config = 
+
+    open MBrace.Core
+    open MBrace.Runtime
+    open MBrace.Thespian
+
+    // change to alter cluster size
+    let private workerCount = 4
+    
+    let mutable private thespian = None
+    /// Gets or creates a new Thespian cluster session.
+    let GetCluster() = 
+        match thespian with 
+        | None -> 
+            let cluster = 
+                ThespianCluster.InitOnCurrentMachine(workerCount, 
+                                                     logger = new ConsoleLogger(), 
+                                                     logLevel = LogLevel.Info)
+            thespian <- Some cluster
+        | Some t -> ()
+        thespian.Value
+
+    /// Kills the current cluster session
+    let KillCluster() =
+        match thespian with
+        | None -> ()
+        | Some t -> t.KillAllWorkers() ; thespian <- None
